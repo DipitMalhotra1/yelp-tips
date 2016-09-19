6d3e1dc24e509c18fba71be3b99de2f4f8a6f69e
Dipit Malhotra <dipitmalhotra@gmail.com>
2016-09-19 11:15:27 -0500

    Specific keyword tips

---
+++ keyword tips.py
@@ -0,0 +1,26 @@
+import nltk
+import json
+import string
+import re
+
+with open("yelp_academic_dataset_review.json") as f:
+	f=open("yelp_academic_dataset_review.json")
+	for line in f:
+		line=line.rstrip()
+		#print line
+		res= re.search("\.([^\.]*between [0-9] am[^\.]*)",line) 
+		res1=re.search("\.([^\.]*between [0-9] pm[^\.]*)",line)
+
+
+		if res is not None:
+			x=str(res.group(1))
+			print "recommended:::",x
+			
+		if res1 is not None:
+			y=str(res1.group(1))
+			print "time:::",y
+	print "********************************"
+
+
+
+
